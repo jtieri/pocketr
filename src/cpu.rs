76203101222cb468a569185f982688a662e1e781
Justin Tieri <37750742+jtieri@users.noreply.github.com>
2026-02-07 00:30:46 -0600

    feat: implement add function for opcodes related to addition instructions
    Still a WIP but just trying to make some movement on implementing
    handlers for more opcodes. A pattern should become more obvious for how
    to abstract things a bit to avoid code duplication

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,6 +5,7 @@ mod flags;
 mod registers;
 mod instructions;
 
+// PREFIX_BYTE is used as a prefix for 16bit opcodes 
 const PREFIX_BYTE: u8 = 0xCB;
 
 #[derive(Debug)]
@@ -38,14 +39,43 @@ impl CPU {
         self.pc = self.execute_opcode(opcode)
     }
     
-    // nop is the NOP CPU instruction that results in a no operation.
+    // nop is the NOP CPU instruction that does nothing (no-op) other than advance the program counter by 1.
     // Opcode: 0x00
-    // Cycles: 1
     // Bytes: 1
-    // Flags: None affected
+    // Cycles: 1
+    // Flags: ----
     fn nop(&mut self) {
     
     }
+    
+    // add is the ADD CPU instruction for adding the value found in an 8bit target register to the value in the A register.
+    // The new value is then stored in the A register.
+    // Opcode: 0x80 - 0x87
+    // Bytes: 1
+    // Cycles: 1
+    // Flags: ZHC-
+    fn add(&mut self) {
+        // TODO: Get target register and implement match to handle all 8bit target registers?
+        // Read value currently in the target register
+        let val = self.registers.c;
+        
+        // Add the target register value to the value in the A register and handle overflow
+        let (new_val, overflow) = self.registers.a.overflowing_add(val);
+
+        // Write updated value to the A register
+        self.registers.a = new_val;
+        
+        // Update flags
+        self.registers.f.set_zero_flag(new_val == 0);
+        self.registers.f.set_subtract_flag(false);
+        self.registers.f.set_carry_flag(overflow);
+        self.registers.f.set_half_carry_flag((self.registers.a & 0xF) + (val & 0xF) > 0xF);
+    }
+    
+    fn did_half_carry() {
+        
+    }
+    
         
     fn execute_opcode(&mut self, opcode: u8) -> u16 {
         if let Some(&instruction_fn) = INSTRUCTIONS.get(opcode as usize) {
